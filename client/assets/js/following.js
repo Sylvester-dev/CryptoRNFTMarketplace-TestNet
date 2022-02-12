f96931c14631448722ab328bab6363a63ecc674a
missioncryptor <missioncryptor@gmail.com>
2022-02-12 12:48:28 +0530

    gg
    2

--- client/assets/js/following.js
+++ client/assets/js/following.js
@@ -1,7 +1,6 @@
-const appId = "tgREnAbuniiRrBV4kRdCxo1jLfisw4JmsG28ToBA"; // Application id from moralis.io
-const serverUrl = 'https://4q2r9curxf6a.usemoralis.com:2053/server'; //Server url from moralis.io
+serverUrl = "https://4q2r9curxf6a.usemoralis.com:2053/server";
+appId = 'tgREnAbuniiRrBV4kRdCxo1jLfisw4JmsG28ToBA';
 Moralis.start({ serverUrl, appId });
-
 
 const user = Moralis.User.current();
 
--- client/assets/js/profile.js
+++ client/assets/js/profile.js
@@ -1,5 +1,5 @@
-const appId = "tgREnAbuniiRrBV4kRdCxo1jLfisw4JmsG28ToBA"; // Application id from moralis.io
-const serverUrl = 'https://4q2r9curxf6a.usemoralis.com:2053/server'; //Server url from moralis.io
+appId = "tgREnAbuniiRrBV4kRdCxo1jLfisw4JmsG28ToBA"; // Application id from moralis.io
+serverUrl = 'https://4q2r9curxf6a.usemoralis.com:2053/server'; //Server url from moralis.io
 Moralis.start({ serverUrl, appId });
 
 
